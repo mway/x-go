ea8d61d6c7c6563a3009c46ed6ca89c18a4aa28c
Matt Way <mway@uber.com>
2022-04-19 16:56:02 -0400

    Add os.WithCwd

---
+++ os/with_cwd.go
@@ -0,0 +1,40 @@
+package os
+
+import (
+	"os"
+)
+
+// WithCwd attempts to change the working directory to dir and, if successful,
+// calls f. WithCwd expects dir to exist already; if dir does not exist, or is
+// removed or renamed during f's execution, an error will be returned.
+//
+// WithCwd will attempt to restore the original working directory, even if the
+// given function panics.
+func WithCwd(dir string, f func()) (err error) {
+	var orig string
+	if orig, err = os.Getwd(); err != nil {
+		return
+	}
+
+	// If we're already in the target directory, just execute f.
+	if dir == orig {
+		f()
+		return
+	}
+
+	if err = os.Chdir(dir); err != nil {
+		return
+	}
+
+	defer func() {
+		err = os.Chdir(orig)
+
+		// Re-panic if we're recovering.
+		if r := recover(); r != nil {
+			panic(r)
+		}
+	}()
+
+	f()
+	return
+}
---
+++ os/with_cwd_test.go
@@ -0,0 +1,113 @@
+package os_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	xos "go.mway.dev/x/os"
+)
+
+func TestWithCwd(t *testing.T) {
+	orig := resolvedTempDir(t)
+	require.NoError(t, os.Chdir(orig))
+	requireCwd(t, orig)
+
+	var (
+		newdir = resolvedTempDir(t)
+		err    = xos.WithCwd(newdir, func() {
+			requireCwd(t, newdir)
+		})
+	)
+
+	require.NoError(t, err)
+	requireCwd(t, orig)
+}
+
+func TestWithCwdPanic(t *testing.T) {
+	orig := resolvedTempDir(t)
+	require.NoError(t, os.Chdir(orig))
+	requireCwd(t, orig)
+
+	var (
+		newdir = resolvedTempDir(t)
+		err    error
+	)
+	require.Panics(t, func() {
+		err = xos.WithCwd(newdir, func() {
+			requireCwd(t, newdir)
+			panic("oops")
+		})
+	})
+	require.NoError(t, err)
+	requireCwd(t, orig)
+}
+
+func TestWithCwdOrigDirGone(t *testing.T) {
+	orig := resolvedTempDir(t)
+	require.NoError(t, os.Chdir(orig))
+	requireCwd(t, orig)
+
+	var (
+		newdir = resolvedTempDir(t)
+		err    = xos.WithCwd(newdir, func() {
+			requireCwd(t, newdir)
+			require.NoError(t, os.Remove(orig))
+		})
+	)
+
+	require.Error(t, err)
+	requireCwd(t, newdir)
+}
+
+func TestWithCwdOrigDirRenamed(t *testing.T) {
+	orig := resolvedTempDir(t)
+	require.NoError(t, os.Chdir(orig))
+	requireCwd(t, orig)
+
+	var (
+		moved  = orig + "-moved"
+		newdir = resolvedTempDir(t)
+		err    = xos.WithCwd(newdir, func() {
+			requireCwd(t, newdir)
+			require.NoError(t, os.Rename(orig, moved))
+		})
+	)
+
+	require.Error(t, err)
+	requireCwd(t, newdir)
+}
+
+func TestWithCwdEmptyTargetDir(t *testing.T) {
+	err := xos.WithCwd("", func() {
+		require.FailNow(t, "WithCwd func argument should not be called")
+	})
+	require.Error(t, err)
+}
+
+func TestWithCwdTargetDirDoesNotExist(t *testing.T) {
+	err := xos.WithCwd("/foo/bar/baz", func() {
+		require.FailNow(t, "WithCwd func argument should not be called")
+	})
+	require.Error(t, err)
+}
+
+// orig dne
+// orig renamed
+
+func resolvedTempDir(t *testing.T) string {
+	dir, err := filepath.Abs(t.TempDir())
+	require.NoError(t, err)
+
+	dir, err = filepath.EvalSymlinks(dir)
+	require.NoError(t, err)
+
+	return dir
+}
+
+func requireCwd(t *testing.T, dir string) {
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+	require.Equal(t, dir, cwd)
+}
