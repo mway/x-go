f7c4ea3af74236e46c57a78fd981cfd35478265e
Matt Way <mway@mway.id>
2023-12-29 18:47:39 -0500

    env: Add package for Var and Path helpers

---
+++ env/path.go
@@ -0,0 +1,170 @@
+// Copyright (c) 2023 Matt Way
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE THE SOFTWARE.
+
+package env
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+const _envKeyPath = "PATH"
+
+// Path represents $PATH within the environment.
+type Path struct {
+	v *Var
+}
+
+// NewPath returns a [Path] that holds the current value of $PATH.
+func NewPath() Path {
+	return Path{
+		v: NewVar(_envKeyPath),
+	}
+}
+
+// Prepend prepends paths to the current value of p.
+func (p Path) Prepend(paths ...string) (newpath Path, err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("failed to prepend path(s): %w", err)
+		}
+	}()
+
+	if len(paths) == 0 {
+		return p, nil
+	}
+
+	// Calculate how much extra storage is needed.
+	extra := len(paths) // for delimiters
+	for _, path := range paths {
+		extra += len(path)
+	}
+
+	// Prepend all of the path fragments to the current value.
+	var buf bytes.Buffer
+	buf.Grow(len(p.v.Value()) + extra)
+	for _, path := range paths {
+		if len(path) == 0 {
+			continue
+		}
+
+		buf.WriteString(path)               //nolint:errcheck
+		buf.WriteByte(os.PathListSeparator) //nolint:errcheck
+	}
+
+	// Write the current value after any prepended path(s).
+	buf.WriteString(p.Value()) //nolint:errcheck
+
+	// Create a copy of this Path and persist the change to the copy (and the
+	// environment).
+	tmp := Path{
+		v: p.v.Clone(),
+	}
+	if err = tmp.v.Set(buf.String()); err != nil {
+		return
+	}
+	return tmp, nil
+}
+
+// MustPrepend calls p.Prepend and panics if it returns an error.
+func (p Path) MustPrepend(paths ...string) Path {
+	newpath, err := p.Prepend(paths...)
+	if err != nil {
+		panic(err)
+	}
+	return newpath
+}
+
+// Append appends paths to the current value of p.
+func (p Path) Append(paths ...string) (newpath Path, err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("failed to append path(s): %w", err)
+		}
+	}()
+
+	if len(paths) == 0 {
+		return p, nil
+	}
+
+	// Calculate how much extra storage is needed.
+	extra := len(paths) // for delimiters
+	for _, path := range paths {
+		extra += len(path)
+	}
+
+	var buf bytes.Buffer
+	buf.Grow(len(p.v.Value()) + extra)
+
+	// Write the current value before any appended path(s).
+	buf.WriteString(p.v.Value()) //nolint:errcheck
+
+	// Append all of the path fragments to the current value.
+	for _, path := range paths {
+		if len(path) == 0 {
+			continue
+		}
+
+		buf.WriteByte(os.PathListSeparator) //nolint:errcheck
+		buf.WriteString(path)               //nolint:errcheck
+	}
+
+	// Create a copy of this Path and persist the change to the copy (and the
+	// environment).
+	tmp := Path{
+		v: p.v.Clone(),
+	}
+	if err = tmp.v.Set(buf.String()); err != nil {
+		return
+	}
+	return tmp, nil
+}
+
+// MustAppend calls p.Append and panics if it returns an error.
+func (p Path) MustAppend(paths ...string) Path {
+	newpath, err := p.Append(paths...)
+	if err != nil {
+		panic(err)
+	}
+	return newpath
+}
+
+// Restore PATH to its original state within the environment.
+func (p Path) Restore() error {
+	return p.v.Restore()
+}
+
+// MustRestore calls p.Restore and panics if it returns an error.
+func (p Path) MustRestore() {
+	if err := p.Restore(); err != nil {
+		panic(err)
+	}
+}
+
+// Value returns the current value of p.
+func (p Path) Value() string {
+	return p.v.Value()
+}
+
+// String returns p as a string.
+func (p Path) String() string {
+	return p.Value()
+}
---
+++ env/var_test.go
@@ -0,0 +1,155 @@
+// Copyright (c) 2023 Matt Way
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE THE SOFTWARE.
+
+package env
+
+import (
+	"errors"
+	"os"
+	"testing"
+	"unsafe"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVar_NotExists(t *testing.T) {
+	key := uuid.New().String()
+
+	v := NewVar(key)
+	require.Equal(t, key, v.Key())
+	require.Empty(t, v.Value())
+	require.False(t, v.exists)
+
+	envValue, exists := os.LookupEnv(v.Key())
+	require.False(t, exists)
+	require.Empty(t, envValue)
+
+	require.NoError(t, v.Set(t.Name()))
+	envValue, exists = os.LookupEnv(v.Key())
+	require.True(t, exists)
+	require.Equal(t, t.Name(), envValue)
+	require.Equal(t, t.Name(), v.Value())
+
+	require.NoError(t, v.Restore())
+	envValue, exists = os.LookupEnv(v.Key())
+	require.False(t, exists)
+	require.Empty(t, envValue)
+	require.Empty(t, v.Value())
+}
+
+func TestVar_Exists(t *testing.T) {
+	key := uuid.New().String()
+
+	// Ensure there is a value to start.
+	require.NoError(t, os.Setenv(key, t.Name()))
+	defer func() {
+		require.NoError(t, os.Unsetenv(key))
+	}()
+
+	v := NewVar(key)
+	require.Equal(t, key, v.Key())
+	require.Equal(t, t.Name(), v.Value())
+	require.True(t, v.exists)
+
+	envValue, exists := os.LookupEnv(v.Key())
+	require.True(t, exists)
+	require.Equal(t, t.Name(), envValue)
+
+	require.NoError(t, v.Set("override"))
+	envValue, exists = os.LookupEnv(v.Key())
+	require.True(t, exists)
+	require.Equal(t, "override", envValue)
+	require.Equal(t, "override", v.Value())
+
+	require.NoError(t, v.Restore())
+	envValue, exists = os.LookupEnv(v.Key())
+	require.True(t, exists)
+	require.Equal(t, t.Name(), envValue)
+	require.Equal(t, t.Name(), v.Value())
+}
+
+func TestVar_Load(t *testing.T) {
+	key := uuid.New().String()
+
+	// Load a previously unset env.
+	v := NewVar(key)
+	defer v.MustUnset()
+
+	require.NoError(t, os.Setenv(key, t.Name()))
+	v.Load()
+	require.Equal(t, t.Name(), v.Value())
+
+	// Load a previously set env.
+	v = NewVar(key)
+	require.Equal(t, t.Name(), v.Value())
+	require.NoError(t, os.Setenv(key, "override"))
+	v.Load()
+	require.Equal(t, "override", v.Value())
+}
+
+func TestVar_Clone(t *testing.T) {
+	var (
+		v1 = NewVar("foo")
+		v2 = v1.Clone()
+	)
+
+	require.Equal(t, unsafe.Pointer(v1), unsafe.Pointer(v1))
+	require.NotEqual(t, unsafe.Pointer(v1), unsafe.Pointer(v2))
+}
+
+func TestVar_Set_Error(t *testing.T) {
+	err := errors.New("setenv error")
+	withStub(&_osSetenv, osSetenvReturning(err), func() {
+		v := NewVar("test")
+		require.ErrorIs(t, v.Set("this will error"), err)
+		require.Panics(t, func() {
+			v.MustSet("this will panic")
+		})
+	})
+}
+
+func TestVar_Restore_Error(t *testing.T) {
+	err := errors.New("setenv/unsetenv error")
+
+	// Does not exist; uses os.Unsetenv
+	withStub(&_osUnsetenv, osUnsetenvReturning(err), func() {
+		v := NewVar("test")
+		require.ErrorIs(t, v.Restore(), err)
+		require.Panics(t, v.MustRestore)
+	})
+
+	// Exists; uses os.Setenv
+	withStub(&_osSetenv, osSetenvReturning(err), func() {
+		require.NoError(t, os.Setenv("test", "test"))
+		v := NewVar("test")
+		require.ErrorIs(t, v.Restore(), err)
+		require.Panics(t, v.MustRestore)
+	})
+}
+
+func TestVar_Unset_Error(t *testing.T) {
+	err := errors.New("setenv error")
+	withStub(&_osUnsetenv, osUnsetenvReturning(err), func() {
+		v := NewVar("test")
+		require.ErrorIs(t, v.Unset(), err)
+		require.Panics(t, v.MustUnset)
+	})
+}
