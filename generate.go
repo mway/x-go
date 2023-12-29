b223d096fffc76f62108cefc8e76425affc266ea
Matt Way <mway@users.noreply.github.com>
2023-12-29 15:07:36 -0500

    testing: Remove package (#2)

--- generate.go
+++
@@ -1,4 +0,0 @@
-package x
-
-//go:generate rm -rf testing/internal/requiremock
-//go:generate mockgen -package requiremock -destination testing/internal/requiremock/requiremock.go github.com/stretchr/testify/require TestingT
