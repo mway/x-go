9d66c524e80c3f46629efc8b8420c2e0b6218b85
Matt Way <mway@users.noreply.github.com>
2025-01-26 14:18:26 -0500

    [breaking] must: Various API updates (#60)

--- must/must.go
+++ must/must.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Matt Way
+// Copyright (c) 2025 Matt Way
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
@@ -23,10 +23,9 @@
 package must
 
 import (
+	"errors"
 	"fmt"
 	"io"
-
-	"go.mway.dev/errors"
 )
 
 // ErrConditionFailed is a base error used in must package function panics, in
@@ -34,11 +33,11 @@ import (
 // due to this package.
 var ErrConditionFailed = errors.New("must: condition failed")
 
-// Must returns value if err is nil, or panics with err otherwise.
-func Must[T any](value T, err error) T {
+// Error returns value if err is nil, or panics with err otherwise.
+func Error[T any](value T, err error) T {
 	if err != nil {
 		panic(fmt.Errorf(
-			"%w: must.Must[%T] given a non-nil error: %w",
+			"%w: must.Error[%T]() given a non-nil error: %w",
 			ErrConditionFailed,
 			value,
 			err,
@@ -47,13 +46,11 @@ func Must[T any](value T, err error) T {
 	return value
 }
 
-// MustBool returns value if ok is true, or panics otherwise.
-//
-//nolint:revive
-func MustBool[T any](value T, ok bool) T {
+// Bool returns value if ok is true, or panics otherwise.
+func Bool[T any](value T, ok bool) T {
 	if !ok {
 		panic(fmt.Errorf(
-			"%w: must.MustBool[%T] given a false boolean value",
+			"%w: must.Bool[%T]() given a false boolean value",
 			ErrConditionFailed,
 			value,
 		))
@@ -61,47 +58,69 @@ func MustBool[T any](value T, ok bool) T {
 	return value
 }
 
-// A Func is any function that returns a T and either an error or a bool.
-type Func[T any] interface {
+// A PredicateFunc returns whether a value should be considered valid.
+type PredicateFunc[T any] interface {
+	~func(T) bool | ~func() bool
+}
+
+// Predicate returns value if pred returns true, or panics otherwise.
+func Predicate[T any, P PredicateFunc[T]](value T, pred P) T {
+	type (
+		FnT = func(T) bool
+		Fn  = func() bool
+	)
+	if fn, ok := any(pred).(FnT); ok {
+		return Bool(value, fn(value))
+	}
+	return Bool(value, any(pred).(Fn)())
+}
+
+// Any delegates to [Must], [MustBool], or [MustPredicate], depending on P,
+// returning the result.
+func Any[T any, P comparable](value T, pred P) T {
+	if err, ok := any(pred).(error); ok {
+		return Error(value, err)
+	}
+
+	switch x := any(pred).(type) {
+	case bool:
+		return Bool(value, x)
+	case func(T) bool:
+		return Predicate(value, x)
+	case func() bool:
+		return Predicate(value, x)
+	default:
+		var zero P
+		return Bool(value, pred == zero)
+	}
+}
+
+// A MustFunc is any function that returns a T and either an error or a bool.
+//
+//nolint:revive
+type MustFunc[T any] interface {
 	~func() (T, error) | ~func() (T, bool)
 }
 
-// MustFunc calls Must with the values returned by fn.
+// Func calls Must with the values returned by fn.
 //
 //nolint:revive
-func MustFunc[T any, F Func[T]](fn F) T {
-	switch fn := any(fn).(type) {
-	case func() (T, error):
-		value, err := fn()
-		if err != nil {
-			panic(fmt.Errorf(
-				"%w: must.MustFunc[%T] received a non-nil error: %w",
-				ErrConditionFailed,
-				value,
-				err,
-			))
-		}
-		return value
-	case func() (T, bool):
-		value, ok := fn()
-		if !ok {
-			panic(fmt.Errorf(
-				"%w: must.MustFunc[%T] received a false boolean value",
-				ErrConditionFailed,
-				value,
-			))
-		}
-		return value
-	default:
-		panic("unreachable")
+func Func[T any, F MustFunc[T]](fn F) T {
+	type (
+		fnError = func() (T, error)
+		fnBool  = func() (T, bool)
+	)
+	if fn, ok := any(fn).(fnError); ok {
+		return Error(fn())
 	}
+	return Bool(any(fn).(fnBool)())
 }
 
 // Do panics if fn returns an error.
 func Do(fn func() error) {
 	if err := fn(); err != nil {
 		panic(fmt.Errorf(
-			"%w: must.Do received a non-nil error: %w",
+			"%w: must.Do() received a non-nil error: %w",
 			ErrConditionFailed,
 			err,
 		))
@@ -112,7 +131,7 @@ func Do(fn func() error) {
 func Close(closer io.Closer) {
 	if err := closer.Close(); err != nil {
 		panic(fmt.Errorf(
-			"%w: must.Close received a non-nil error when closing: %w",
+			"%w: must.Close() received a non-nil error when closing: %w",
 			ErrConditionFailed,
 			err,
 		))
